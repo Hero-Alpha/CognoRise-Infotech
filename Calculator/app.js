3369eab2709c4a94f8a164f19e591e96c98e4ad4
Hero-Alpha <yash2909.yk@gmail.com>
2024-04-01 21:14:56 +0530

    code modification/optimiaztion

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,93 +1,29 @@
-//this is practically the exact code that Brian Holt wrote on the Front End Masters course
+let input = document.querySelector('.calc-numbers');
+let buttons = document.querySelectorAll('button');
 
-let currentTotal = 0; 
-let buffer = "0"; 
-
-let previousOperator = null;
-
-const calcScreen = document.querySelector(".calc-numbers");
-
-document.querySelector('.calc-buttons').addEventListener("click",function(event){
-
-    buttonClick(event.target.innerHTML);
-});
-
-function buttonClick(value){
-    if(isNaN(parseInt(value))){
-        handleSymbol(value);
-    }else{
-        handleNumber(value);
-    }
-    rerenderScreen();
-}
-
-function handleSymbol(value){
-    switch (value){
-        case "AC":
-            buffer = "0";
-            currentTotal = 0;
-            previousOperator = null;
-            break;
-        case "=":
-            if(previousOperator === null){ 
-                return;
-            }
-            flushOperation(parseInt(buffer));
-            buffer = "" + currentTotal;
-            previousOperator = null;
-            currentTotal = 0;
-            break;
-        case "←":
-            if(buffer.length === 1){ 
-                buffer = "0";
-            }
-            else{
-                buffer = buffer.substring(0,buffer.length-1); 
-            }
-            break;
-        default:
-            handleMath(value);
-            break;
-    }
-}
-
-function handleNumber(value){
-    if(buffer === "0"){
-        buffer = value;
-    }else{
-        buffer += value;
-    }
-}
-
-function handleMath(value){
-    const internalBuffer = parseInt(buffer);
-    
-    if (currentTotal === 0){
-        currentTotal = internalBuffer;
-    }else{
-        flushOperation(internalBuffer);
-    }
-
-    previousOperator = value;
-
-    buffer = "0";
-}
-
-function flushOperation(internalBuffer){
-    if(previousOperator === "+"){
-        currentTotal += internalBuffer;
-    }
-    else if(previousOperator === "-"){
-        currentTotal -= internalBuffer;
-    }
-    else if(previousOperator === "x"){
-        currentTotal *= internalBuffer;
-    }
-    else{
-        currentTotal /= internalBuffer;
-    }
-}
-
-function rerenderScreen(){
-    calcScreen.value = buffer;
-}
+let inputString = "";
+let result = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            let expression = inputString.replace(/×/g, '*').replace(/÷/g, '/');
+            result = eval(expression);
+            input.value = result;
+            inputString=result.toString();
+        }
+        else if (e.target.innerHTML == 'AC') {
+            inputString = "";
+            result="";
+            input.value = inputString;
+        }
+        else if (e.target.innerHTML == '←') {
+            inputString = inputString.slice(0, -1);
+            input.value = inputString;
+        }
+        else {
+            inputString += e.target.innerHTML;
+            input.value = inputString;
+        }
+    })
+})
